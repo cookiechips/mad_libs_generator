faaf4f7bc4990372e2af8a9a9514722f9769f029
cookie <test@gmail.com>
2022-06-26 18:29:04 +0300

    Mad Libs Generator

---
+++ mad_libs_generator.py
@@ -0,0 +1,20 @@
+while True:
+
+    color = input("Choose a color: ")
+
+    p_noun = input("Choose a plural noun: ")
+
+    noun = input("Choose a noun: ")
+
+    adjective = input("Choose an adjective (Describing word): ")
+
+
+    print('==================================================================')
+
+    print(f'Roses are {color.capitalize()},')
+    print(f'{p_noun.capitalize()} are blue,')
+    print(f'{noun.capitalize()} is {adjective.capitalize()},  ')
+    print('and so are you.')
+    print('==================================================================')
+
+    break
